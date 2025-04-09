860dae623ba2db84261549508cdf40d768fa2a47
MagicSanti <126059104+MagicSanti@users.noreply.github.com>
2025-04-09 16:34:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -45,5 +46,6 @@ def home():
     return render_template('index.html', result_message=None, encrypted_text=None)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 10000))  # Render uses port 10000 by default
+    app.run(host='0.0.0.0', port=port)
